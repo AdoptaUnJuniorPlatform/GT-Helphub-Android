2ac1db01680e4988ebe010bc42a597aa21fd2a14
AlejandroRP <romeroporras94@gmail.com>
2024-09-29 16:31:18 +0200

    Añadidos colores Helphub

--- app/src/main/java/com/alejandro/helphub/ui/theme/Theme.kt
+++ app/src/main/java/com/alejandro/helphub/ui/theme/Theme.kt
@@ -12,15 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = White,
+    surface = PurpleBackground,
+    primary = PurpleMain,
+    secondary = Black
 )
 
+
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = White,
+    surface = PurpleBackground,
+    primary = PurpleMain,
+    secondary = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
