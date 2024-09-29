18c13f6fc442daec31426388e5736b072bd82a33
AlejandroRP <romeroporras94@gmail.com>
2024-09-29 20:27:29 +0200

    SplashScreen UI

--- app/src/main/java/com/alejandro/helphub/features/splash/presentation/SplashScreen.kt
+++ app/src/main/java/com/alejandro/helphub/features/splash/presentation/SplashScreen.kt
@@ -1,31 +1,23 @@
 package com.alejandro.helphub.features.splash.presentation
 
-import androidx.annotation.ColorRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.alejandro.helphub.R
-import com.alejandro.helphub.ui.theme.PurpleBackground
 
 @Preview
 @Composable
