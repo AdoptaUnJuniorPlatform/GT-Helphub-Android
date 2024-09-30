2e9b0083e0eb63a7309a39ba221c0352b1074505
AlejandroRP <romeroporras94@gmail.com>
2024-09-30 13:02:20 +0200

    Navigation implemented in MainActivity

--- app/src/main/java/com/alejandro/helphub/MainActivity.kt
+++ app/src/main/java/com/alejandro/helphub/MainActivity.kt
@@ -11,13 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.alejandro.helphub.navigation.RootNavGraph
 import com.alejandro.helphub.ui.theme.HelphubTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController= rememberNavController()
+            RootNavGraph(navController = navController)
             HelphubTheme {
                // Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
