cae45df77b5fb12ea6bebe92275f64bdbb3626c6
Maria Laura Luraschi <lurmarialaura@gmail.com>
2024-11-03 12:10:04 +0100

    plugin de SonarQube

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.hilt) apply false
+    id("org.sonarqube") version "5.1.0.4882"
+}
 
+sonar {
+    properties {
+        property("sonar.projectKey", "helphub-android")
+        property("sonar.projectName", "Helphub Android")
+    }
 }
